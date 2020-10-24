72ad8c1c26ed91775a9f1ea6722c9a1a9005e8d7
Jesus Mejia <jesusmejded@gmail.com>
2020-10-23 23:47:00 -0500

    dummy notebook

---
+++ introduccion.jl
@@ -0,0 +1,26 @@
+### A Pluto.jl notebook ###
+# v0.12.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 77d2834a-15b3-11eb-39c0-b31321d4a0d7
+md"""# Introducción a Julia
+
+Espero que les guste este tutorial
+"""
+
+# ╔═╡ a197ee5e-15b3-11eb-051f-11a3c0c94ad0
+a = 1
+
+# ╔═╡ a7dac908-15b3-11eb-2dad-1db1cdd77935
+b = 1.0
+
+# ╔═╡ ad72c474-15b3-11eb-379e-077cc3ba871e
+
+
+# ╔═╡ Cell order:
+# ╟─77d2834a-15b3-11eb-39c0-b31321d4a0d7
+# ╠═a197ee5e-15b3-11eb-051f-11a3c0c94ad0
+# ╠═a7dac908-15b3-11eb-2dad-1db1cdd77935
+# ╠═ad72c474-15b3-11eb-379e-077cc3ba871e
